0a63aeafa47290ae97c3a4861fac94e4e25f749f
grayette <74732813+grayette@users.noreply.github.com>
2021-01-27 15:42:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,6 +21,10 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind-speed");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
+    let precipitationElement = document.querySelector("#precipitation");
+    
+    celsiusTemperature = response.data.main.temp;
+    
     temperatureElement.innerHTML = Math.round (response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     description.innerHTML = response.data.weather[0].description;
@@ -45,7 +49,32 @@ function handleSubmit(event) {
     search(cityInputElement.value);
 }
 
-search("New York");
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("San Diego");
